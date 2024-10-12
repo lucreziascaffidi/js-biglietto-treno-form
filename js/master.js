4d1cef5b25c413be4d36db6970ccd3c5e07c59da
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-12 15:09:04 +0200

    feat: finished project and readme

--- js/master.js
+++ js/master.js
@@ -3,26 +3,11 @@ console.log('sono collegato');
 //Prendo il form
 const myForm = document.getElementById('calculated-form');
 
-// Prendo i campi
-const userKm = document.getElementById('user-KM');
-const numberUserKm = Number(userKm);
-
-const userAge = document.getElementById('user-age').value;
-const numberUserAge = Number(userAge);
-const resultContainer = document.getElementById('show-price');
-const resultText = document.getElementById('result');
-
-// variabili prezzo
-const oneKmPrice = 0.21;
-const ticketPrice = (oneKmPrice * numberUserKm);
-
-// variabili età
-const under18 = (numberUserAge < 18);
-const over65 = (numberUserAge > 65);
-
-// variabili sconto
-const ticketPriceUnder18 = (ticketPrice * 20) / 100;
-const ticketPriceOver65 = (ticketPrice * 40) / 100;
+// // Prendo i campi del form
+// const userKm = document.getElementById('user-KM').value;
+// console.log(userKm);
+// const userAge = document.getElementById('user-age').value;
+// console.log(userAge);
 
 myForm.addEventListener('submit', function (event) {
 
@@ -30,7 +15,29 @@ myForm.addEventListener('submit', function (event) {
 
     console.log('prova invio form');
 
+    // Prendo i campi del form 
+    const userKm = document.getElementById('user-KM').value;
+    const numberUserKm = Number(userKm);
+    console.log(numberUserKm);
+    const userAge = document.getElementById('user-age').value;
+    const numberUserAge = Number(userAge);
+    console.log(numberUserAge);
 
+    // variabili prezzo
+    const oneKmPrice = 0.21;
+    const ticketPrice = (oneKmPrice * numberUserKm);
+    console.log(ticketPrice)
+
+    // variabili età
+    const under18 = (numberUserAge < 18);
+    const over65 = (numberUserAge > 65);
+
+    // variabili sconto
+    const ticketPriceUnder18 = (ticketPrice * 20) / 100;
+    const ticketPriceOver65 = (ticketPrice * 40) / 100;
+
+    const resultContainer = document.getElementById('show-price');
+    const resultText = document.getElementById('result');
 
     //calcolo del prezzo
     if (under18) {
