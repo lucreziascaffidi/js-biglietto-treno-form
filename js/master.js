0c462e2ae9f43c2c339fd2bff84558feee5971fb
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2024-10-12 12:50:57 +0200

    feat: added logic

--- js/master.js
+++ js/master.js
@@ -1,69 +1,50 @@
-console.log('Hello World')
+console.log('sono collegato');
 
-// Prendere il form dal DOM
+//Prendo il form
 const myForm = document.getElementById('calculated-form');
 
-// Prendere i campi del Form
-const userKM = document.getElementById('user-KM');
-const userAge = document.getElementById('user-age');
+// Prendo i campi
+const userKm = document.getElementById('user-KM');
+const numberUserKm = Number(userKm);
 
+const userAge = document.getElementById('user-age').value;
+const numberUserAge = Number(userAge);
+const resultContainer = document.getElementById('show-price');
+const resultText = document.getElementById('result');
 
-// Inizializzo la variabile relativa al prezzo del biglietto per 1 Km
-// e le assegno il numero 0.21 considerando che il costo è di 0.21€ per 1 km. 
+// variabili prezzo
 const oneKmPrice = 0.21;
+const ticketPrice = (oneKmPrice * numberUserKm);
 
-// Inizializzo la variabile relativa al costo dell'interno biglietto
-// moltiplicando 0.21 per i KM indicati dall'utente nella prima domanda e associati alla variabile del punto 1.
-const ticketPrice = (oneKmPrice * toTravelKM);
+// variabili età
+const under18 = (numberUserAge < 18);
+const over65 = (numberUserAge > 65);
 
-// Inizializzo la variabile dell'età inferiore ai 18 anni
-const under18 = (userAge < 18);
-
-// Inizializzo la variabile del costo del bigletto scontato del 20% per l'età è inferiore ai 18 anni. 
+// variabili sconto
 const ticketPriceUnder18 = (ticketPrice * 20) / 100;
-
-//Inizializzo la variabile dell'età maggiore di 65 anni
-const over65 = (userAge > 65);
-
-// Inizializzo la variabile del costo del bigletto scontato del 40% per l'età maggiore di 65 anni. 
 const ticketPriceOver65 = (ticketPrice * 40) / 100;
 
-// quando l'user invia il form, leggo i dati 
 myForm.addEventListener('submit', function (event) {
 
-    //decidiamo il comportamento del form al click del bottone submit
     event.preventDefault();
 
-    console.log('prova invio form')
+    console.log('prova invio form');
 
-    console.log('km', userKM.value);
-    console.log('age', userAge.value);
 
-    // quando ho finito operazioni i campi devono tornare vuoti
-    userKM.value = '';
+
+    //calcolo del prezzo
+    if (under18) {
+        resultText.innerHTML = ticketPriceUnder18.toFixed(2);
+    } else if (over65) {
+        resultText.innerHTML = ticketPriceOver65.toFixed(2);
+    } else {
+        resultText.innerHTML = ticketPrice.toFixed(2);
+    }
+
+    //svuoto i campi de form
+    userKm.value = '';
     userAge.value = '';
-});
 
-
-//Istruzioni condizionali: 
-
-// se l'età è inferiore ai 18 anni 
-// if (under18) {
-
-//     // allora il costo del biglietto è scontato del 20% quindi faccio apparire questa frase
-//     console.log(`Il costo totale del biglietto è di ${ticketPriceUnder18.toFixed(2)}€. Scontato del 20% per i minorenni.`)
-
-//     //altrimenti se l'età è maggiore di 65 anni  
-// } else if (over65) {
-
-//     // allora il costo del biglietto è scontato del 40% e faccio apparire questa frase
-//     console.log(`Il costo totale del biglietto è di ${ticketPriceOver65.toFixed(2)}€. Scontato del 40% per gli over 65.`)
-
-//     // altrimenti l'età è sicuramente compresa tra i 18 e i 65 anni
-// } else {
-
-//     // allora l'utente non ha diritto a nessuno sconto e faccio apparire questa frase
-//     console.log(`Il costo totale del biglietto è di ${ticketPrice.toFixed(2)}€. L'utente non ha diritto a nessuno sconto.`)
-
-// }
-
+    // mostro i risultati
+    resultContainer.classList.remove('d-none');
+})
